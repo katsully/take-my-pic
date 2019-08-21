be1dda1174a1a63781cddca2fd5f3daeef241ad6
Kat Sullivan <kmsullivan012@gmail.com>
2019-08-21 17:40:18 -0400

    first commit

---
+++ src/sanfran.py
@@ -0,0 +1,236 @@
+from statistics import mode
+
+import cv2
+from keras.models import load_model
+import numpy as np
+
+from utils.datasets import get_labels
+from utils.inference import detect_faces
+from utils.inference import draw_text
+from utils.inference import draw_bounding_box
+from utils.inference import apply_offsets
+from utils.inference import load_detection_model
+from utils.preprocessor import preprocess_input
+
+import dlib
+
+def landmarks_to_np(landmarks, dtype="int"):
+    # 获取landmarks的数量
+    num = landmarks.num_parts
+    
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((num, 2), dtype=dtype)
+    
+    # loop over the 68 facial landmarks and convert them
+    # to a 2-tuple of (x, y)-coordinates
+    for i in range(0, num):
+        coords[i] = (landmarks.part(i).x, landmarks.part(i).y)
+    # return the list of (x, y)-coordinates
+    return coords
+
+def get_centers(img, landmarks):
+    # 线性回归
+    EYE_LEFT_OUTTER = landmarks[2]
+    EYE_LEFT_INNER = landmarks[3]
+    EYE_RIGHT_OUTTER = landmarks[0]
+    EYE_RIGHT_INNER = landmarks[1]
+
+    x = ((landmarks[0:4]).T)[0]
+    y = ((landmarks[0:4]).T)[1]
+    A = np.vstack([x, np.ones(len(x))]).T
+    k, b = np.linalg.lstsq(A, y, rcond=None)[0]
+    
+    x_left = (EYE_LEFT_OUTTER[0]+EYE_LEFT_INNER[0])/2
+    x_right = (EYE_RIGHT_OUTTER[0]+EYE_RIGHT_INNER[0])/2
+    LEFT_EYE_CENTER =  np.array([np.int32(x_left), np.int32(x_left*k+b)])
+    RIGHT_EYE_CENTER =  np.array([np.int32(x_right), np.int32(x_right*k+b)])
+    
+    pts = np.vstack((LEFT_EYE_CENTER,RIGHT_EYE_CENTER))
+    cv2.polylines(img, [pts], False, (255,0,0), 1) #画回归线
+    cv2.circle(img, (LEFT_EYE_CENTER[0],LEFT_EYE_CENTER[1]), 3, (0, 0, 255), -1)
+    cv2.circle(img, (RIGHT_EYE_CENTER[0],RIGHT_EYE_CENTER[1]), 3, (0, 0, 255), -1)
+    
+    return LEFT_EYE_CENTER, RIGHT_EYE_CENTER
+
+def get_aligned_face(img, left, right):
+    desired_w = 256
+    desired_h = 256
+    desired_dist = desired_w * 0.5
+    
+    eyescenter = ((left[0]+right[0])*0.5 , (left[1]+right[1])*0.5)# 眉心
+    dx = right[0] - left[0]
+    dy = right[1] - left[1]
+    dist = np.sqrt(dx*dx + dy*dy)# 瞳距
+    scale = desired_dist / dist # 缩放比例
+    angle = np.degrees(np.arctan2(dy,dx)) # 旋转角度
+    M = cv2.getRotationMatrix2D(eyescenter,angle,scale)# 计算旋转矩阵
+
+    # update the translation component of the matrix
+    tX = desired_w * 0.5
+    tY = desired_h * 0.5
+    M[0, 2] += (tX - eyescenter[0])
+    M[1, 2] += (tY - eyescenter[1])
+
+    aligned_face = cv2.warpAffine(img,M,(desired_w,desired_h))
+    
+    return aligned_face
+
+#==============================================================================
+#   4.是否戴眼镜判别函数
+#       输入：对齐后的人脸图片
+#       输出：判别值(True/False)
+#============================================================================== 
+def judge_eyeglass(img):
+    img = cv2.GaussianBlur(img, (11,11), 0) 
+
+    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0 ,1 , ksize=-1) 
+    sobel_y = cv2.convertScaleAbs(sobel_y) 
+    cv2.imshow('sobel_y',sobel_y)
+
+    edgeness = sobel_y 
+    
+    retVal,thresh = cv2.threshold(edgeness,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    d = len(thresh) * 0.5
+    x = np.int32(d * 6/7)
+    y = np.int32(d * 3/4)
+    w = np.int32(d * 2/7)
+    h = np.int32(d * 2/4)
+
+    x_2_1 = np.int32(d * 1/4)
+    x_2_2 = np.int32(d * 5/4)
+    w_2 = np.int32(d * 1/2)
+    y_2 = np.int32(d * 8/7)
+    h_2 = np.int32(d * 1/2)
+    
+    roi_1 = thresh[y:y+h, x:x+w] 
+    roi_2_1 = thresh[y_2:y_2+h_2, x_2_1:x_2_1+w_2]
+    roi_2_2 = thresh[y_2:y_2+h_2, x_2_2:x_2_2+w_2]
+    roi_2 = np.hstack([roi_2_1,roi_2_2])
+    
+    measure_1 = sum(sum(roi_1/255)) / (np.shape(roi_1)[0] * np.shape(roi_1)[1])#计算评价值
+    measure_2 = sum(sum(roi_2/255)) / (np.shape(roi_2)[0] * np.shape(roi_2)[1])#计算评价值
+    measure = measure_1*0.3 + measure_2*0.7
+    
+    cv2.imshow('roi_1',roi_1)
+    cv2.imshow('roi_2',roi_2)
+    # print(measure)
+    
+   
+    # Determine the discriminant value based on the relationship 
+    # between the evaluation value and the threshold
+    if measure > 0.15:
+        judge = True
+    else:
+        judge = False
+    # print(judge)
+    return judge
+
+
+# parameters for loading data and images
+detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
+emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
+emotion_labels = get_labels('fer2013')
+gender_labels = get_labels('imdb')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# hyper-parameters for bounding boxes shape
+frame_window = 10
+gender_offsets = (30, 60)
+emotion_offsets = (20, 40)
+
+# loading models
+face_detection = load_detection_model(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+gender_classifier = load_model(gender_model_path, compile=False)
+
+# eyeglasses
+predictor_path = "../data/shape_predictor_5_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+# getting input model shapes for inference
+emotion_target_size = emotion_classifier.input_shape[1:3]
+gender_target_size = gender_classifier.input_shape[1:3]
+
+# starting lists for calculating modes
+gender_window = []
+emotion_window = []
+
+# starting video streaming
+cv2.namedWindow('window_frame')
+video_capture = cv2.VideoCapture(0)
+if video_capture.isOpened(): # try to get the first frame
+    rval, bgr_image = video_capture.read()
+else:
+    rval = False
+while rval:
+    rval, bgr_image = video_capture.read()
+    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+    faces = detect_faces(face_detection, gray_image)
+
+    for face_coordinates in faces:
+
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
+        rgb_face = rgb_image[y1:y2, x1:x2]
+
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+        gray_face = gray_image[y1:y2, x1:x2]
+        try:
+            rgb_face = cv2.resize(rgb_face, (gender_target_size))
+            gray_face = cv2.resize(gray_face, (emotion_target_size))
+        except:
+            continue
+        gray_face = preprocess_input(gray_face, False)
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+        emotion_text = emotion_labels[emotion_label_arg]
+        emotion_window.append(emotion_text)
+
+        rgb_face = np.expand_dims(rgb_face, 0)
+        rgb_face = preprocess_input(rgb_face, False)
+        gender_prediction = gender_classifier.predict(rgb_face)
+        gender_label_arg = np.argmax(gender_prediction)
+        gender_text = gender_labels[gender_label_arg]
+        gender_window.append(gender_text)
+
+        if len(gender_window) > frame_window:
+            emotion_window.pop(0)
+            gender_window.pop(0)
+        try:
+            emotion_mode = mode(emotion_window)
+            gender_mode = mode(gender_window)
+        except:
+            continue
+
+        if gender_text == gender_labels[0]:
+            color = (0, 0, 255)
+        else:
+            color = (255, 0, 0)
+
+        ## eyeglasses
+        landmarks = predictor(gray_image, face_coordinates)
+        landmarks = landmarks_to_np(landmarks)
+        LEFT_EYE_CENTER, RIGHT_EYE_CENTER = get_centers(bgr_image, landmarks)
+        aligned_face = get_aligned_face(gray_image, LEFT_EYE_CENTER, RIGHT_EYE_CENTER)
+        judge = judge_eyeglass(aligned_face)
+        if judge == True:
+            print("Glasses")
+        else:
+            print("No Glasses")
+
+        draw_bounding_box(face_coordinates, rgb_image, color)
+        draw_text(face_coordinates, rgb_image, gender_mode,
+                  color, 0, -20, 1, 1)
+        draw_text(face_coordinates, rgb_image, emotion_mode,
+                  color, 0, -45, 1, 1)
+
+    bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('window_frame', bgr_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
