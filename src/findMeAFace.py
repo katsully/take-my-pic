c974f8988338972b56d071f6b669e53761e66f01
Kat Sullivan <kmsullivan012@gmail.com>
2019-09-11 19:07:22 -0400

    able to work on more than one face, does cropping to track single face

--- src/findMeAFace.py
+++ src/findMeAFace.py
@@ -8,15 +8,15 @@ from pythonosc import udp_client
 import subprocess
 import time
 import imutils
-from statistics import mode
 
 from utils.datasets import get_labels
 from utils.inference import apply_offsets
 from utils.preprocessor import preprocess_input
-from utils.glasses_helper import landmarks_to_np
-from utils.glasses_helper import get_centers
-from utils.glasses_helper import get_aligned_face
-from utils.glasses_helper import judge_eyeglass
+from utils.kats_helper import landmarks_to_np
+from utils.kats_helper import get_centers
+from utils.kats_helper import get_aligned_face
+from utils.kats_helper import judge_eyeglass
+from utils.kats_helper import get_color_name
 
 cam = cv2.VideoCapture(0)
 cam.set(3,640)	# width
@@ -37,10 +37,14 @@ client = udp_client.UDPClient("127.0.0.1", 8001)
 # counter for time with no faces
 counter = 0
 # counter for collecting the avg info about a person
-avg_counter = 0
+avg_counter = face_counter = 0
 emotion_text = []
 gender_text = []
 wearing_glasses = []
+shirt_color = []
+found_face = True;
+face_x = face_y = face_w = face_h = 0
+face_analyze = False
 
 # hyper-parameters for bounding boxes shape
 gender_offsets = (30, 60)
@@ -53,6 +57,8 @@ gender_classifier = load_model(gender_model_path, compile=False)
 # getting input model shapes for inference
 emotion_target_size = emotion_classifier.input_shape[1:3]
 gender_target_size = gender_classifier.input_shape[1:3]
+
+
 
 
 if cam.isOpened(): # try to get the first frame
@@ -78,11 +84,15 @@ while(ret):
 	# when faces are detected, the faces variable is an array of tuple, when no faces are detected the faces variable is an empty tuple
 	if isinstance(faces, tuple):
 		counter+=1
+		found_face = False
+		face_analyze = False
+		face_counter = 0
 		# reset average face counter
 		avg_counter = 0
-		emotion_text = []
-		gender_text = []
-		wearing_glasses = []
+		emotion_text.clear()
+		gender_text.clear()
+		wearing_glasses.clear()
+		shirt_color.clear()
 		# only send message if no faces detected after 10 seconds
 		if(counter > 200):
 			msg = osc_message_builder.OscMessageBuilder(address="/noFace")
@@ -94,58 +104,160 @@ while(ret):
 	else:
 		# reset noFaces timer
 		counter = 0
-		for (x,y,w,h) in faces: 
-			x1,x2,y1,y2 = apply_offsets((x,y,w,h), emotion_offsets)
-			gray_face = gray_img[y1:y2, x1:x2]
-			x1,x2,y1,y2 = apply_offsets((x,y,w,h), gender_offsets)
-			rgb_face_og = rgb_img[y1:y2, x1:x2]
-			try:
-				gray_face = cv2.resize(gray_face, (emotion_target_size))
-				rgb_face = cv2.resize(rgb_face_og, (gender_target_size))
-			except:
-				continue
-			# get emotion
-			gray_face = preprocess_input(gray_face, False)
-			gray_face = np.expand_dims(gray_face, 0)
-			gray_face = np.expand_dims(gray_face, -1)
-			emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
-			emotion_text.append(emotion_labels[emotion_label_arg])
+		# focusing on a single face
+		if found_face:
+			x1,x2,y1,y2 = apply_offsets((face_x, face_y, face_w, face_h), gender_offsets)
+			# crop image so we only focus on this face
+			cropped_img = gray_img[y1:y2, x1:x2]
+			faces = face_detector.detectMultiScale(cropped_img, scaleFactor=1.3, minNeighbors=5)
+			# is the face gone?
+			if isinstance(faces, tuple):
+				found_face = False
+				face_counter = 0
+				print("lost your face!")
+			# face is still there
+			else:
+				# if we're still determining this face isn't someone quickly entering and exiting
+				if not face_analyze:
+					face_counter += 1
+					# face isn't just coming and going
+					if face_counter > 25:
+						face_analyze = True
+				# we're ready to analyze this face!
+				if face_analyze:
+					print("analyzing face!")
+					x1,x2,y1,y2 = apply_offsets((face_x, face_y, face_w, face_h), emotion_offsets)
+					gray_face_og = gray_img[y1:y2, x1:x2]
+					x1,x2,y1,y2 = apply_offsets((face_x, face_y, face_w, face_h), gender_offsets)
+					rgb_face_og = rgb_img[y1:y2, x1:x2]
+					try:
+						gray_face = cv2.resize(gray_face_og, (emotion_target_size))
+						rgb_face = cv2.resize(rgb_face_og, (gender_target_size))
+					except:
+						continue
+					# get emotion
+					gray_face = preprocess_input(gray_face, False)
+					gray_face = np.expand_dims(gray_face, 0)
+					gray_face = np.expand_dims(gray_face, -1)
+					emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+					emotion_text.append(emotion_labels[emotion_label_arg])
 
-			# get gender
-			rgb_face = np.expand_dims(rgb_face, 0)
-			rgb_face = preprocess_input(rgb_face, False)
-			gender_prediction = gender_classifier.predict(rgb_face)
-			gender_label_arg = np.argmax(gender_prediction)
-			gender_text.append(gender_labels[gender_label_arg])
+					# get gender
+					rgb_face = np.expand_dims(rgb_face, 0)
+					rgb_face = preprocess_input(rgb_face, False)
+					gender_prediction = gender_classifier.predict(rgb_face)
+					gender_label_arg = np.argmax(gender_prediction)
+					gender_text.append(gender_labels[gender_label_arg])
 
-			# eyeglasses
-			rect = dlib.rectangle(x,y,x+w,y+h)
-			landmarks = predictor(gray_img, rect)
-			landmarks = landmarks_to_np(landmarks)
-			LEFT_EYE_CENTER, RIGHT_EYE_CENTER = get_centers(flipped, landmarks)
-			aligned_face = get_aligned_face(gray_img, LEFT_EYE_CENTER, RIGHT_EYE_CENTER)
-			wearing_glasses.append(judge_eyeglass(aligned_face))
+					# eyeglasses
+					rect = dlib.rectangle(x,y,x+w,y+h)
+					landmarks = predictor(gray_img, rect)
+					landmarks = landmarks_to_np(landmarks)
+					LEFT_EYE_CENTER, RIGHT_EYE_CENTER = get_centers(flipped, landmarks)
+					aligned_face = get_aligned_face(gray_img, LEFT_EYE_CENTER, RIGHT_EYE_CENTER)
+					wearing_glasses.append(judge_eyeglass(aligned_face))
 
-			avg_counter += 1
-			print (avg_counter)
+					avg_counter += 1
 
-			if avg_counter > 50:
-				print("HERE")
-				# get timestamp
-				ts = time.gmtime()
-				timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", ts)
-				fileName = "../faces/face" + timestamp + ".jpg"
-				cv2.imwrite(fileName, rgb_face_og)
-				caption = mode(emotion_text) + " " + mode(gender_text)
-				if mode(wearing_glasses):
-					caption += " and is wearing glasses"
-				subprocess.call([r'C:/Users/NUC6-USER/take-my-pic/insta.bat', fileName, caption])
-				# exit the loop
-				ret = False
-				avg_counter = 0
-				emotion_text = []
-				gender_text = []
-				wearing_glasses = []
+					# shirt color
+					# s_y = int(h * 1.25)
+					# s_h = int(h * 1.25)
+					# shirt_region = rgb_img[s_y:s_y+s_h, x:x+w]
+					# b, g, r = np.mean(shirt_region, axis=(0,1))
+					# actual_name, closest_name = get_color_name((r,g,b))
+					# print(actual_name, closest_name)
+
+					if avg_counter > 50:
+						print("send face to instaaaaa")
+						# get timestamp
+						ts = time.gmtime()
+						timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", ts)
+						fileName = "../faces/face" + timestamp + ".jpg"
+						cv2.imwrite(fileName, rgb_face_og)
+						caption = max(set(emotion_text), key=emotion_text.count) + " " + max(set(gender_text), key=gender_text.count)
+						if max(set(wearing_glasses), key=wearing_glasses.count):
+							caption += " and is wearing glasses"
+						subprocess.call([r'C:/Users/NUC6-USER/take-my-pic/insta.bat', fileName, caption])
+						# exit the loop
+						ret = False
+						# Reset everything
+						avg_counter = 0
+						emotion_text.clear()
+						gender_text.clear()
+						wearing_glasses.clear()
+						shirt_color.clear()
+						found_face = face_analyze = False
+						face_counter = 0
+				
+		# still looking for a face to focus on
+		else:	
+			for (x,y,w,h) in faces: 
+				# we found a face!
+				print("found a face")
+				found_face = True;
+				face_x, face_y, face_w, face_h = x,y,w,h
+				break
+
+				# x1,x2,y1,y2 = apply_offsets((x,y,w,h), emotion_offsets)
+				# gray_face_og = gray_img[y1:y2, x1:x2]
+				# x1,x2,y1,y2 = apply_offsets((x,y,w,h), gender_offsets)
+				# rgb_face_og = rgb_img[y1:y2, x1:x2]
+				# try:
+				# 	gray_face = cv2.resize(gray_face_og, (emotion_target_size))
+				# 	rgb_face = cv2.resize(rgb_face_og, (gender_target_size))
+				# except:
+				# 	continue
+				# # get emotion
+				# gray_face = preprocess_input(gray_face, False)
+				# gray_face = np.expand_dims(gray_face, 0)
+				# gray_face = np.expand_dims(gray_face, -1)
+				# emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+				# emotion_text.append(emotion_labels[emotion_label_arg])
+
+				# # get gender
+				# rgb_face = np.expand_dims(rgb_face, 0)
+				# rgb_face = preprocess_input(rgb_face, False)
+				# gender_prediction = gender_classifier.predict(rgb_face)
+				# gender_label_arg = np.argmax(gender_prediction)
+				# gender_text.append(gender_labels[gender_label_arg])
+
+				# # eyeglasses
+				# rect = dlib.rectangle(x,y,x+w,y+h)
+				# landmarks = predictor(gray_img, rect)
+				# landmarks = landmarks_to_np(landmarks)
+				# LEFT_EYE_CENTER, RIGHT_EYE_CENTER = get_centers(flipped, landmarks)
+				# aligned_face = get_aligned_face(gray_img, LEFT_EYE_CENTER, RIGHT_EYE_CENTER)
+				# wearing_glasses.append(judge_eyeglass(aligned_face))
+
+				# avg_counter += 1
+				# print(avg_counter)
+
+				# shirt color
+				# s_y = int(h * 1.25)
+				# s_h = int(h * 1.25)
+				# shirt_region = rgb_img[s_y:s_y+s_h, x:x+w]
+				# b, g, r = np.mean(shirt_region, axis=(0,1))
+				# actual_name, closest_name = get_color_name((r,g,b))
+				# print(actual_name, closest_name)
+
+				# if avg_counter > 50:
+				# 	print("HERE")
+				# 	# get timestamp
+				# 	ts = time.gmtime()
+				# 	timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", ts)
+				# 	fileName = "../faces/face" + timestamp + ".jpg"
+				# 	cv2.imwrite(fileName, rgb_face_og)
+				# 	caption = mode(emotion_text) + " " + mode(gender_text)
+				# 	if mode(wearing_glasses):
+				# 		caption += " and is wearing glasses"
+				# 	subprocess.call([r'C:/Users/NUC6-USER/take-my-pic/insta.bat', fileName, caption])
+				# 	# exit the loop
+				# 	ret = False
+				# 	avg_counter = 0
+				# 	emotion_text = []
+				# 	gender_text = []
+				# 	wearing_glasses = []
+				# 	shirt_color = []
 
 	cv2.imshow('test window', flipped)
 	k = cv2.waitKey(30 & 0xff)
--- src/utils/kats_helper.py
+++ src/utils/kats_helper.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import webcolors
 
 def landmarks_to_np(landmarks, dtype="int"):
     # landmarks
@@ -106,3 +107,21 @@ def judge_eyeglass(img):
         judge = False
     # print(judge)
     return judge
+
+def closest_color(requested_color):
+    min_colors = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
+
+def get_color_name(requested_color):
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_color)
+    except ValueError:
+        closest_name = closest_color(requested_color)
+        actual_name = None
+    return actual_name, closest_name
