a9e24100d2e21e23193e6a608acd3c64a652cad9
Kat Sullivan <kmsullivan012@gmail.com>
2019-09-21 16:20:56 -0400

    osc receiver working and insta screen updated

--- src/InstaScreen.py
+++ src/InstaScreen.py
@@ -35,37 +35,36 @@ def update_screen():
 	# cv2.destroyAllWindows()
 	all_photos = glob.glob('../faces/*')
 	most_recent = max(all_photos, key=os.path.getctime)
-	print(most_recent)
 	recent_image = cv2.imread(most_recent)
-	recent_height, recent_width = recent_image.shape[:2]
+	h, w = recent_image.shape[:2]
+	cropped = recent_image[0:h,int(w*.08):int(w*.92)]
+	c_h, c_w = cropped.shape[:2]
+	new_height = int((c_h*(1080/c_w)) - (1080*.1))
+	resized_img = cv2.resize(cropped, (1080, new_height))
+	recent_height, recent_width = resized_img.shape[:2]
 
-	# go through other photos
-	rows = []
-	for idx,filename in enumerate(all_photos[1:]):
-		if idx % 5 == 0:
-			if idx != 0:
-				rows.append(smaller_pic)
-			row_concat = cv2.imread(filename)
-			smaller_pic = cv2.resize(row_concat, (225, 225), interpolation=cv2.INTER_AREA)
-		else:
-			temp_img = cv2.imread(filename)
-			temp_smaller = cv2.resize(temp_img, (225, 225), interpolation=cv2.INTER_AREA)
-			smaller_pic = np.concatenate((smaller_pic, temp_smaller), axis=1)
-
-	it = iter(rows)
-	for row1,row2 in zip(it, it):
-		grid_images = np.concatenate((row1, row2), axis=0)
+	smaller_pic_size = int(1080/3)
+	resized_pics = []
+	for i in range(3):
+		pic = cv2.imread(all_photos[i+1])
+		pic = cv2.copyMakeBorder(pic, 0, int(1080*.1), 0, 0, borderType=cv2.BORDER_CONSTANT, value=(255,255,255))
+		resized_pics.append(cv2.resize(pic, (smaller_pic_size, smaller_pic_size)))
+	
+	grid_img = np.concatenate((resized_pics[0], resized_pics[1]), axis=1)
+	grid_img = np.concatenate((grid_img, resized_pics[2]), axis=1)
 
 	# white padding around center of most recent image
-	grid_height, grid_width = grid_images.shape[:2]
+	grid_height, grid_width = grid_img.shape[:2]
 	padding = int((grid_width - recent_width) / 2)
+	vert_padding = int((1920 - (grid_height + recent_height)) / 2)
 	if padding < (grid_width - recent_width) / 2:
-		padded_recent_img = cv2.copyMakeBorder(recent_image, 0, 0, padding, padding+1, borderType=cv2.BORDER_CONSTANT, value=(255,255,255))
+		padded_recent_img = cv2.copyMakeBorder(resized_img, vert_padding, vert_padding, padding, padding+1, borderType=cv2.BORDER_CONSTANT, value=(255,255,255))
 	else:
-		padded_recent_img = cv2.copyMakeBorder(recent_image, 0, 0, padding, padding, borderType=cv2.BORDER_CONSTANT, value=(255,255,255))
+		padded_recent_img = cv2.copyMakeBorder(resized_img, vert_padding, vert_padding, padding, padding, borderType=cv2.BORDER_CONSTANT, value=(255,255,255))
 
 	
-	final_img = np.concatenate((padded_recent_img, grid_images), axis=0)
+	final_img = np.concatenate((padded_recent_img, grid_img), axis=0)
+	final_img = cv2.line(final_img, (0,int(1920*.78)), (1080,int(1920*.78)), (200,200,200), 2) 
 
 	return final_img	
 
