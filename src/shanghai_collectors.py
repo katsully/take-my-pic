1ad987b54d1f18f2da21df278be097448cb547dc
Kat Sullivan <kmsullivan012@gmail.com>
2021-07-07 09:06:49 -0800

    selfie option

--- src/shanghai_collectors.py
+++ src/shanghai_collectors.py
@@ -22,75 +22,73 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1920)
 
 x1 = x2 = y1 = y2 = 0
-
 flipped = {}
-
 moment_time = False
+selfie = False
 
 # build udp_client for osc protocol
 osc_client = udp_client.UDPClient("127.0.0.1", 8001)
 
 def take_photo(address, *args):
-    global x1
-    global x2
-    global y1
-    global y2
-    global flipped
-    global moment_time
-
-    # convert image to 1:1 aspect ratio                     
-    # x1 -= (x2-x1) * .25
-    # x2 += (x2-x1) * .25
-    # y1 -= (y2-y1) * .1
-    # y2 += (y2-y1) * .4
-    # flipped_h, flipped_w = flipped.shape[:2]
-    # if x1 < 0:
-    #     x1 = 0
-    # if x2 >= flipped_w:
-    #     x2 = flipped_w -1
-    # if y1 < 0:
-    #     y1 = 0
-    # if y2 >= flipped_h:
-    #     y2 = flipped_h -1
-    # portrait_img = flipped[int(y1):int(y2), int(x1):int(x2)]
-    # scale_percent = 225 # percent of original size
-    # bigger_width = int(portrait_img.shape[1] * scale_percent / 100)
-    # bigger_height = int(portrait_img.shape[0] * scale_percent / 100)
-    # dim = (bigger_width, bigger_height)
-    # # resize image
-    # resized = cv2.resize(portrait_img, dim, interpolation = cv2.INTER_CUBIC)
-    # aspect_ratio_h, aspect_ratio_w = resized.shape[:2]
-    # crop image to be square
-    # new_height = aspect_ratio_w 
-    # final_img = resized[0:int(new_height), 0:aspect_ratio_w]
-    # save file to faces database
-    # cv2.imwrite(file_name, final_img)
+    global x1, x2, y1, y2   # coordinates surrounding the face
+    global flipped          # matrix with the camera view rotated and flipped
+    global moment_time      # boolean stating whether avatar can do a 'moment'
+    global selfie
 
     img_h, img_w = flipped.shape[:2]
-    remaining_y_space = img_w - (y2 - y1)
-    if y1 - (remaining_y_space/2) < 0:
-        remaining_y_space -= y1
-        y1 = 0
-        y2 += remaining_y_space
-    elif y + (remaining_y_space/2) > img_h:
-        remaining_y_space -= img_h - y2
-        y2 - img_h
-        y1 -= remaining_y_space
+
+    if selfie:
+        remaining_y_space = (img_w - (y2 - y1))
+        if y1 - (remaining_y_space/2) < 0:
+            remaining_y_space -= y1
+            y1 = 0
+            y2 += remaining_y_space
+        elif y2 + (remaining_y_space/2) > img_h:
+            remaining_y_space -= (img_h - y2)
+            y2 = img_h
+            y1 -= remaining_y_space
+        else:
+            y2 += remaining_y_space / 2
+            y1 -= remaining_y_space / 2
+        crop_img = flipped[y1:y2, 0:img_w]
+
+        pil_img = cv2.cvtColor(crop_img,cv2.COLOR_BGR2RGB)
+        pilimg = Image.fromarray(pil_img)
+
+        overlay = Image.open('CarliSelfie.png')
+
+        pilimg.paste(overlay, (0,0), mask = overlay)
+
+        final_img = cv2.cvtColor(np.array(pilimg),cv2.COLOR_RGB2BGR)                     
+        selfie = False
+
     else:
-        y2 += remaining_y_space / 2
-        y1 -= remaining_y_space / 2
-    crop_img = flipped[y1:y2, 0:img_w]
+        # convert image to 1:1 aspect ratio                     
+        x1 -= (x2-x1) * .25
+        x2 += (x2-x1) * .25
+        y1 -= (y2-y1) * .1
+        y2 += (y2-y1) * .4
+        if x1 < 0:
+            x1 = 0
+        if x2 >= img_w:
+            x2 = img_w -1
+        if y1 < 0:
+            y1 = 0
+        if y2 >= img_h:
+            y2 = img_h -1
+        portrait_img = flipped[int(y1):int(y2), int(x1):int(x2)]
+        scale_percent = 225 # percent of original size
+        bigger_width = int(portrait_img.shape[1] * scale_percent / 100)
+        bigger_height = int(portrait_img.shape[0] * scale_percent / 100)
+        dim = (bigger_width, bigger_height)
+        # resize image
+        resized = cv2.resize(portrait_img, dim, interpolation = cv2.INTER_CUBIC)
+        aspect_ratio_h, aspect_ratio_w = resized.shape[:2]
+        # crop image to be square
+        new_height = aspect_ratio_w 
+        final_img = resized[0:int(new_height), 0:aspect_ratio_w]     
 
-    pil_img = cv2.cvtColor(crop_img,cv2.COLOR_BGR2RGB)
-    pilimg = Image.fromarray(pil_img)
-
-    overlay = Image.open('CarliSelfie.png')
-
-    pilimg.paste(overlay, (0,0), mask = overlay)
-
-    cv2img = cv2.cvtColor(np.array(pilimg),cv2.COLOR_RGB2BGR)
-
-    cv2.imwrite("../faces/photo.png", cv2img)
+    cv2.imwrite("../faces/photo.png", final_img) 
     print("saved photo")
     moment_time = True
 
@@ -103,16 +101,13 @@ def moments_enabled(arg):
     msg = msg.build()
     osc_client.send(msg)
 
-
 async def face_finding():
 
     global cam
-    global x1
-    global x2
-    global y1
-    global y2
+    global x1, x2, y1, y2
     global flipped
     global moment_time
+    global selfie
 
     face_detector = cv2.CascadeClassifier('../trained_models/detection_models/haarcascade_frontalface_default.xml')
     emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
@@ -121,12 +116,11 @@ async def face_finding():
     # hyper-parameters for bounding boxes shape
     crop_offsets = (50, 70)
     emotion_offsets = (20, 40)
-
     emotion_classifier = load_model(emotion_model_path, compile=False)      # loading models
     emotion_target_size = emotion_classifier.input_shape[1:3]       # getting input model shapes for inference
-
    
     capture_counter = 0     # keep track of capturing every third face
+    selfie_counter = 0      # keep track of when we do selfie vs 'normal' pic
     tracking_faces = True   # whether we are tracking faces or letting the avatars do an animation (ie a 'moment')
 
     # counter for collecting the avg info about a person
@@ -151,7 +145,6 @@ async def face_finding():
 
     if cam.isOpened(): # try to get the first frame
         ret, img = cam.read()   
-
     else:
         ret = False
 
@@ -231,11 +224,20 @@ async def face_finding():
                             if avg_counter > 30:  
                                 if capture_counter % 3 == 0:  
                                     # tell matt to take a photo
-                                    print("sending to matt to take pic")
-                                    msg = osc_message_builder.OscMessageBuilder(address="/takeAPic")
-                                    msg.add_arg(0)
-                                    msg = msg.build()
-                                    osc_client.send(msg)
+                                    if selfie_counter % 3 == 0:
+                                        selfie = True
+                                        print("sending to matt to take selfie pic")
+                                        msg = osc_message_builder.OscMessageBuilder(address="/takeAPicSelfie")
+                                        msg.add_arg(0)
+                                        msg = msg.build()
+                                        osc_client.send(msg)
+                                    else:
+                                        print("sending to matt to take pic")
+                                        msg = osc_message_builder.OscMessageBuilder(address="/takeAPic")
+                                        msg.add_arg(0)
+                                        msg = msg.build()
+                                        osc_client.send(msg)
+                                    selfie_counter += 1
 
                                     # stop searching for faces until matt takes photo
                                     tracking_faces = False
@@ -294,8 +296,7 @@ async def face_finding():
                 break
         # time right after photo is taken, where the avatar will do an animation/moment
         elif moment_time:
-            rand_num = randint(60,90)
-            t_end = time() + rand_num
+            t_end = time() + randint(60,90)
             while time() < t_end:
                 moments_enabled(1)
             moments_enabled(0)
